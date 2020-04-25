17bad50de31be4d1f07235cf93eecf7f9272e62f
mich2k <miky3848@gmail.com>
2020-04-25 14:06:27 +0200

    introducing whitespace skip, not working yet

--- textsplitter.py
+++ textsplitter.py
@@ -1,22 +1,39 @@
 
 from pathlib import Path
 import pathlib
-
+import tempfile
 
 def main():
     print("Remember to add file extension   (.log, .txt..) ")
     src_filename = input("file name: ")    # custom input
     # src_filename = "your_text_file.log"  # hard coding
     fr = open(src_filename, "r")
+    ch = input(
+        "Would you like to exclude whitespaces\whitelines?\nSuggested 'y'\n[y\\n]    ")
+    if(not(ch == 'y' or ch == 'n')):
+        print("Error")
+        return
     path = str(pathlib.Path().parent.absolute()) + "/" + src_filename
     noext_filename = Path(path).resolve().stem
     ext = str(pathlib.Path(src_filename).suffix)
-    i = k = 0
+    if(ch == 'y'):
+        ftmp = tempfile.TemporaryFile(mode='w+')
+    i = k = totlines = 0
     cont = 1
     for line in fr:
-        totlines += 1
-    print("Found " + str(i) + " lines.")
+        if(ch == 'y' and not(line.isspace())):
+            ftmp.write(line)
+            totlines += 1
+            continue
+        elif(ch == 'n'):
+            totlines += 1
+    for l in ftmp:
+        print(l)
+    print("Found " + str(totlines) + " lines.")
     part = int(input("In how many partitions do you want to split the file: "))
+    if(part > totlines):
+        print("Error")
+        return
     val = int(totlines/part)
     fr.seek(0, 0)
     while (cont <= part):
@@ -30,6 +47,8 @@ def main():
         f.close()
         cont += 1
     fr.close()
+    if(ch == 'y'):
+        ftmp.close()
 
 if(__name__ == "__main__"):
     main()
