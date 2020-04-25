b6ce2c11bc85ec63b167e80ad3f6be4e6c6c139d
mich2k <miky3848@gmail.com>
2020-04-25 03:21:54 +0200

    first commit

---
+++ text_splitter.py
@@ -0,0 +1,36 @@
+
+from pathlib import Path
+import pathlib
+
+
+def main():
+    print("Remember to add file extension   (.log, .txt..) ")
+    src_filename = input("file name: ")    # custom input
+    # src_filename = "../rockyou_utf8.inp"  # hard coding
+    fr = open(src_filename, "r")
+    path = str(pathlib.Path().parent.absolute()) + "/" + src_filename
+    noext_filename = Path(path).resolve().stem
+    ext = str(pathlib.Path(src_filename).suffix)
+    i = 0
+    cont = 1
+    k = 0
+    for line in fr:
+        i += 1
+    print(i)
+    part = int(input("In how many partitions do you want to split the file: "))
+    val = int(i/part)
+    fr.seek(0, 0)
+    while (cont <= part):
+        f = open(noext_filename + "-part-" + str(cont) + ext, "w")
+        while(k < val*cont):
+            line = str(fr.readline())
+            if not line:
+                break
+            f.write(line)
+            k += 1
+        f.close()
+        cont += 1
+
+
+if(__name__ == "__main__"):
+    main()
