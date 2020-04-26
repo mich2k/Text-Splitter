c047e8ab8715af368decf0c7715fe19be3d4da9d
mich2k <miky3848@gmail.com>
2020-04-27 01:57:54 +0200

    hotfixes

--- textsplitter.py
+++ textsplitter.py
@@ -8,28 +8,30 @@ import tempfile
 def print_mult(x, MAX):
     cont = 0
     arr=set()
-    for i in range (2,x):
+    for i in range (2,x+1):
         if(x%i==0):
             cont+=1
+            arr.add(i)
             if(cont > MAX):
                 break
-            arr.add(i)
     print(arr)
     return
 
-def partitions(reading_stream, ch, ftmp):
+def partitions(reading_stream, select, ftmp, totlines):
     # looking how many lines are
     for line in reading_stream:
-        if(ch == 'y' and not(line.isspace())):
+        if(select == 'y' and not(line.isspace())):
             ftmp.write(line)
             totlines += 1
-        elif(ch == 'n'):
+        elif(select == 'n'):
             totlines += 1
-    ftmp.seek(0, 0)
+    if(select == 'y'):
+        ftmp.seek(0, 0)
     MAX = 15 #  maximum number of suggested partitions
     print("Found " + str(totlines) + " lines.")
-    part = int(input("In how many partitions do you want to split the file? "))
-    print("Here you are " + str(MAX) + " suggested legit partitions to use: ")
+    print("\nHere you are some suggested legit partition size\\s to use: ")
+    print_mult(totlines,MAX)
+    part = int(input("\nIn how many partitions do you want to split the file? "))
     if(part>totlines != 0):
         print("Error: partitions greater than totlines!")
         quit()
@@ -42,30 +44,33 @@ def inputf():
     print("Remember to add file extension   (.log, .txt..) ")
     src_filename = input("file name: ")
     reading_stream = open(src_filename, "r")
-    ch = input(
+    select = input(
         "Would you like to exclude whitespaces\\whitelines?\nSuggested 'y'\n[y\\n]    ")
-    if(not(ch == 'y' or ch == 'n')):
+    if(not(select == 'y' or select == 'n')):
         print("Error")
         quit()
     path = str(pathlib.Path().parent.absolute()) + "/" + src_filename
     noext_filename = Path(path).resolve().stem
     ext = str(pathlib.Path(src_filename).suffix)
-    if(ch == 'y'):
+    if(select == 'y'):
         ftmp = tempfile.TemporaryFile(mode='w+')
-    return ch, reading_stream, noext_filename, ext, ftmp
+        return select, reading_stream, noext_filename, ext, ftmp
+    else:
+        return select, reading_stream, noext_filename, ext, False
 
 def main():
-    ch, reading_stream, noext_filename, ext, ftmp = inputf()
-    part, totlines = partitions(reading_stream, ch, ftmp)
+    select, reading_stream, noext_filename, ext, ftmp = inputf()
+    part, totlines = partitions(reading_stream, select, ftmp, 0)
     val = int(totlines/part)
-    k = totlines = 0
+    k = 0
     cont = 1
     reading_stream.seek(0, 0)
-    ftmp.seek(0, 0)
+    if(select == 'y'):
+        ftmp.seek(0, 0)
     while (cont <= part):
         f = open(noext_filename + "-part-" + str(cont) + ext, "w")
         while(k < val):
-            if(ch == 'n'):
+            if(select == 'n'):
                 f.write(str(reading_stream.readline()))
             else:
                 f.write(str(ftmp.readline()))
@@ -74,7 +79,7 @@ def main():
         cont += 1
         val *= cont
     reading_stream.close()
-    if(ch == 'y'):
+    if(select == 'y'):
         ftmp.close()
     return
 
